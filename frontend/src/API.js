26cc86359c9ee3adaf5234bc1351129176c85a98
SaiTimmaReddy <saithimma@techis.io>
2022-01-11 19:21:05 +0530

    updated the backend url

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,7 +7,7 @@ var baseURL;
 // } else {
 //     baseURL = 'http://127.0.0.1:8000';
 // }
-baseURL = 'https://backend-bebe.herokuapp.com/';
+baseURL = 'https://backend-angel-bouquet.herokuapp.com/';
 
 const api = axios.create({
     baseURL: baseURL,
